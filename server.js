0197b3fdcb4fa3dc2fff2d22ff359a3591fa2d3a
Emily Reed <emilyreed89@gmail.com>
2022-06-25 18:00:36 -0400

    fixed error server running and db connected

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-require('express')
+const express = require('express')
 const app = express()
 const cors = require('cors')
 const { ppid } = require('process')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+// const PORT = 8004
 
 let db,
     dbConnectionString = process.env.DB_STRING,
@@ -16,3 +17,7 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('todotasks')
     })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port 8004`)
+})
